b94c8dc106eb9097f17a52b795190261af21eee7
Zdorov Roman <rmzdorov@yandex.ru>
2025-11-16 22:53:10 +0300

    подготовка

---
+++ src/TMultiStack.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include "TMultiStack.h"
+
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}
---
+++ test/test_TMultiStack.cpp
@@ -0,0 +1,4 @@
+#include "TMultiStack.h"
+#include <gtest.h>
+#include <fstream>
+#include <sstream>
